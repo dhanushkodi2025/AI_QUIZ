656e36969233f3f6f016378c16fd6c4385841bc9
dhanushkodi2035 <dhanushkodi20042020@gmaail.com>
2025-07-01 11:57:24 +0530

    testing commit

--- server/index.js
+++ server/index.js
@@ -3,22 +3,20 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const passport = require("passport");
 const cookieParser = require("cookie-parser");
-
 dotenv.config();
-
 const app = express();
 
 app.use(cors({
   origin: process.env.FRONTEND_URL,
   credentials: true
 }));
+
 app.use(cookieParser());
 app.use(express.json());
 app.use(passport.initialize());
-
 require("./middlewares/passport");
 
 const authRoutes = require("./routes/authRoutes");
 app.use("/auth", authRoutes);
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+app.listen(5000, () => console.log("This Server running on port 5000"));
